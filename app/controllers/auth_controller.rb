8c67ec8d141026b95562e53a80d06589784a5180
Drew Keat <thedrewkeat@gmail.com>
2022-06-07 21:37:12 -0600

    Create auth_controller and add routes for login/logout

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,22 @@
+class AuthController < ApplicationController
+  def create
+    @user = User.find_by(email: auth_params[:email])
+    if @user.authenticate(auth_params[:password])
+      render json: UserSerializer.new(@user), status: :accepted, location: @user
+    else
+      render json: {message: "Unable to verify account"}, status: 401
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def auth_params
+    params.require(:user).permit(:id, :email, :password)
+  end
+  
+
+end
